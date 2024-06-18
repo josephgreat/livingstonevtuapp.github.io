0a729cbc450f8273c426f9ac921f614c9c4c0443
Joseph <josephgreat2003@gmail.com>
2024-06-18 17:37:57 +0100

    commented deferred prompt in app.js

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ if ("serviceWorker" in navigator) {
     })
   }
 
-  let deferredPrompt;
+//   let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  document.getElementById('installButton').style.display = 'block';
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   e.preventDefault();
+//   deferredPrompt = e;
+//   document.getElementById('installButton').style.display = 'block';
 
-  document.getElementById('installButton').addEventListener('click', () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
-});
+//   document.getElementById('installButton').addEventListener('click', () => {
+//     deferredPrompt.prompt();
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//       if (choiceResult.outcome === 'accepted') {
+//         console.log('User accepted the A2HS prompt');
+//       } else {
+//         console.log('User dismissed the A2HS prompt');
+//       }
+//       deferredPrompt = null;
+//     });
+//   });
+// });
