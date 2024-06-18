bb016b74b98920496a5a92b7319016d0fdbd7ac3
Joseph <josephgreat2003@gmail.com>
2024-06-18 17:34:43 +0100

    added screenshots to manifest

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ if ("serviceWorker" in navigator) {
         .catch(err => console.log("service worker not registered", err))
     })
   }
+
+  let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  document.getElementById('installButton').style.display = 'block';
+
+  document.getElementById('installButton').addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
